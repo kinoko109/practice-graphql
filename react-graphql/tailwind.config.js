2dde92cfe85f95bba431c0e368bc2aecfd1faa9c
kinoko109 <tatsuaki1009@gmail.com>
2024-09-16 23:10:10 +0900

    fix tailwind setting

---
+++ react-graphql/tailwind.config.js
@@ -0,0 +1,20 @@
+// import type { Config } from "tailwindcss";
+//
+// const tailwindConfig: Config = {
+//   content: ["./src/**/*.{ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+//
+// export default tailwindConfig;
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
